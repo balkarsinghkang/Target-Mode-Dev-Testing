7676eb905a4a867c069c64e6816293976d11d618
Balkar Singh Kang <balkar.singh@progress.com>
2024-03-26 09:33:21 -0500

    Push updates
    Signed-off-by: Balkar Singh Kang <balkar.singh@progress.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,9 +4,15 @@
 #
 # Copyright:: 2024, The Authors, All Rights Reserved.
 
+# gem_package 'cisco_node_utils' 
+# do
+#   source '/Users/balkarsinghkang/Downloads/cisco_node_utils-2.1.0.gem'
+# end
+
+
 cisco_motd '192.168.240.5' do
   username 'admin'
   password 'Training'
   enable_password 'Training'
-  motd 'Configured via Chef recipe Balkar and Anil'
+  motd 'Configured via Chef recipe Balkar'
 end
