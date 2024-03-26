82befba4d51ac84578f90357e7266e5019ea32e7
Balkar Singh Kang <balkar.singh@progress.com>
2024-03-26 09:57:53 -0500

    testing on linux with gem
    Signed-off-by: Balkar Singh Kang <balkar.singh@progress.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2024, The Authors, All Rights Reserved.
 
-# gem_package 'cisco_node_utils' 
+gem_package 'cisco_node_utils' 
 # do
 #   source '/Users/balkarsinghkang/Downloads/cisco_node_utils-2.1.0.gem'
 # end
