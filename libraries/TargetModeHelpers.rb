dafaec937811c3d451e4e597d967efa2865a5728
Balkar Singh Kang <balkar.singh@progress.com>
2024-03-28 22:49:49 -0500

    testing cisco enable mode
    Signed-off-by: Balkar Singh Kang <balkar.singh@progress.com>

---
+++ libraries/TargetModeHelpers.rb
@@ -0,0 +1,73 @@
+#
+# Chef Infra Documentation
+# https://docs.chef.io/libraries/
+#
+
+#
+# This module name was auto-generated from the cookbook name. This name is a
+# single word that starts with a capital letter and then continues to use
+# camel-casing throughout the remainder of the name.
+#
+module TargetModeHelpers
+  module TargetmodehelpersHelpers
+    #
+    # Define the methods that you would like to assist the work you do in recipes,
+    # resources, or templates.
+    #
+    class TargetModeHelperClass
+      def empty(x)
+        (!x.nil? and x.strip.empty?)
+      end
+
+      def initialize(backend)
+        @backend = backend
+      end
+
+      def run_command(command, *args, debug: false)
+        unix_cmd = format(command, *args)
+        cmd = @backend.run_command(unix_cmd)
+
+        if debug
+          puts ''
+          puts '**************************************************'
+          puts 'TargetModeHelper.run_command'
+          puts format('Command: %s', unix_cmd)
+          puts format('stdout: %s', cmd.stdout)
+          puts format('stderr: %s', cmd.stderr)
+          puts '**************************************************'
+        end
+
+        cmd.stdout
+      end
+
+      def read_text_file(file)
+        unix_cmd = format('cat %s || echo -n', file)
+        @backend.run_command(unix_cmd).stdout
+      end
+
+      def write_text_file(file, contents)
+        encoded = Base64.strict_encode64(contents)
+        unix_cmd = format("echo '%<base64>s' | base64 --decode > %<file>s", base64: encoded, file: file)
+        @backend.run_command(unix_cmd).stdout
+      end
+    end
+  end
+end
+
+#
+# The module you have defined may be extended within the recipe to grant the
+# recipe the helper methods you define.
+#
+# Within your recipe you would write:
+#
+#     extend ::TargetmodehelpersHelpers
+#
+#     my_helper_method
+#
+# You may also add this to a single resource within a recipe:
+#
+#     template '/etc/app.conf' do
+#       extend ::TargetmodehelpersHelpers
+#       variables specific_key: my_helper_method
+#     end
+#
