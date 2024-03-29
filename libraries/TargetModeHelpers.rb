db393ec56e53fd7cbc8f6a44964054e364eaef96
Balkar Singh Kang <balkar.singh@progress.com>
2024-03-28 23:12:02 -0500

    testing cisco enable mode
    Signed-off-by: Balkar Singh Kang <balkar.singh@progress.com>

--- libraries/TargetModeHelpers.rb
+++ libraries/TargetModeHelpers.rb
@@ -9,47 +9,45 @@
 # camel-casing throughout the remainder of the name.
 #
 module TargetModeHelpers
-  module TargetmodehelpersHelpers
-    #
-    # Define the methods that you would like to assist the work you do in recipes,
-    # resources, or templates.
-    #
-    class TargetModeHelperClass
-      def empty(x)
-        (!x.nil? and x.strip.empty?)
+  #
+  # Define the methods that you would like to assist the work you do in recipes,
+  # resources, or templates.
+  #
+  class TargetModeHelperClass
+    def empty(x)
+      (!x.nil? and x.strip.empty?)
+    end
+
+    def initialize(backend)
+      @backend = backend
+    end
+
+    def run_command(command, *args, debug: false)
+      unix_cmd = format(command, *args)
+      cmd = @backend.run_command(unix_cmd)
+
+      if debug
+        puts ''
+        puts '**************************************************'
+        puts 'TargetModeHelper.run_command'
+        puts format('Command: %s', unix_cmd)
+        puts format('stdout: %s', cmd.stdout)
+        puts format('stderr: %s', cmd.stderr)
+        puts '**************************************************'
       end
 
-      def initialize(backend)
-        @backend = backend
-      end
+      cmd.stdout
+    end
 
-      def run_command(command, *args, debug: false)
-        unix_cmd = format(command, *args)
-        cmd = @backend.run_command(unix_cmd)
+    def read_text_file(file)
+      unix_cmd = format('cat %s || echo -n', file)
+      @backend.run_command(unix_cmd).stdout
+    end
 
-        if debug
-          puts ''
-          puts '**************************************************'
-          puts 'TargetModeHelper.run_command'
-          puts format('Command: %s', unix_cmd)
-          puts format('stdout: %s', cmd.stdout)
-          puts format('stderr: %s', cmd.stderr)
-          puts '**************************************************'
-        end
-
-        cmd.stdout
-      end
-
-      def read_text_file(file)
-        unix_cmd = format('cat %s || echo -n', file)
-        @backend.run_command(unix_cmd).stdout
-      end
-
-      def write_text_file(file, contents)
-        encoded = Base64.strict_encode64(contents)
-        unix_cmd = format("echo '%<base64>s' | base64 --decode > %<file>s", base64: encoded, file: file)
-        @backend.run_command(unix_cmd).stdout
-      end
+    def write_text_file(file, contents)
+      encoded = Base64.strict_encode64(contents)
+      unix_cmd = format("echo '%<base64>s' | base64 --decode > %<file>s", base64: encoded, file: file)
+      @backend.run_command(unix_cmd).stdout
     end
   end
 end
