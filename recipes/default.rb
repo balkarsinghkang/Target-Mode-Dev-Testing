7787d437fa6adbcdf7c41e6a0dcaba91e3d7462b
Anil Choudhary <anil.choudhary@tesco.com>
2024-04-03 01:07:19 +0530

    Fixing the TargetModeHelper transport connection issue
    Signed-off-by: Anil Choudhary <anil.choudhary@tesco.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,9 +4,13 @@
 #
 # Copyright:: 2024, The Authors, All Rights Reserved.
 
+require 'pry'
+
 chef_gem 'cisco_node_utils' do
   source '/root/cisco_node_utils-2.1.1.gem'
 end
+
+binding.pry
 
 cisco_motd '192.168.240.5' do
   # username 'admin'
