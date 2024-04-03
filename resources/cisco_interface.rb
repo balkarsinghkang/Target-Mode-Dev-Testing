5b468f4a76f04091869dfe14acc584230a06a148
Balkar Singh Kang <balkar.singh@progress.com>
2024-04-03 16:13:56 -0500

    testing cisco enable mode
    Signed-off-by: Balkar Singh Kang <balkar.singh@progress.com>

--- resources/cisco_interface.rb
+++ resources/cisco_interface.rb
@@ -1,6 +1,6 @@
 require_relative '../libraries/TargetModeHelpers' unless defined?(TargetModeHelpers::TargetModeHelper)
 require 'pry'
-require 'json'
+# require 'json'
 resource_name :cisco_interfdace
 provides :cisco_interface, target_mode: true
 unified_mode true
@@ -13,7 +13,7 @@ property :enabled, String, required: true
 
 load_current_value do |new_resource|
     backend = TargetModeHelpers::TargetModeHelper.new(__transport_connection)
-    int_cfg = JSON.parse(backend.run_command("show running-config interface #{new_resource.interface}"))
+    int_cfg = backend.run_command("show running-config interface #{new_resource.interface}")
     binding.pry
   end
 
