4c1b5e06ba8cefb671cf6be085df2401a24d6104
Balkar Singh Kang <balkar.singh@progress.com>
2024-04-03 22:57:43 -0500

    testing cisco enable mode
    Signed-off-by: Balkar Singh Kang <balkar.singh@progress.com>

--- resources/cisco_interface.rb
+++ resources/cisco_interface.rb
@@ -4,7 +4,7 @@ require 'pry'
 resource_name :cisco_interfdace
 provides :cisco_interface, target_mode: true
 unified_mode true
-default_action :set
+default_action :update
 
 property :interface, String, name_property: true
 property :target, String, required: true
@@ -20,16 +20,21 @@ load_current_value do |new_resource|
     int_cfg = backend.run_command("show running-config interface #{new_resource.interface}")
     description int_cfg.split("\r\n").find { |item| item.lstrip.start_with?("description")}.lstrip.match(/description "(.*?)"/)[1]
     shutdown string_not_nil?(int_cfg.split("\r\n").find { | str | str.match(/\s*shutdown/)})
-    binding.pry
   end
 
 action :update do
+  backend = TargetModeHelpers::TargetModeHelper.new(__transport_connection)
+  backend.run_command('config t')
+  backend.run_command("interface #{new_resource.interface}")
+  
+  converge_if_changed :description do
+    backend.run_command("description #{new_resource.description}")
+  end
 
-#   converge_if_changed :description do
-#     backend = TargetModeHelpers::TargetModeHelper.new(__transport_connection)
-#     backend.run_command('config t')
-#     backend.run_command("banner motd # #{new_resource.motd} #")
-#     backend.run_command('exit')
-#     backend.run_command('wr mem')
-#   end 
+  converge_if_changed :shutdown do
+    backend.run_command("shutdown") if new_resource.shutdown == true
+  end
+  backend.run_command('exit')
+  backend.run_command('exit')
+  backend.run_command('wr mem')
 end
